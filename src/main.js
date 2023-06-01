4ea3dd6282e4be2d09748776956089d3e2fc6cd0
Kimberly Rv <elliis.rv@gmail.com>
2023-06-01 00:30:52 -0600

    añadí botones de categorías y botón para regresar, se modificó css, se crearon las const de los elementos de html

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { example } from './data.js';
+//funcion de data
 
-import data from './data/ghibli/ghibli.js';
-
+import data from './data/ghibli/ghibli.js'; //Importa la data de ghibli y la imprime en consola
 console.log(example, data);
+
+//Header//
+const buscadorInput = document.getElementById("textInput") //Buscador
+const buscarBtn = document.getElementById("botonBuscar") //Botón buscar
+
+//Categorías//
+const directoresBtn = document.getElementById("directores"); //Botón directores
+const duracionBtn = document.getElementById("directores"); //Botón duración
+const añoBtn = document.getElementById("directores"); //Botón año
+const locacionesBtn = document.getElementById("directores"); //Botón locaciones
+const vehiculosBtn = document.getElementById("directores"); //Botón vehiculos
+const premiadosBtn = document.getElementById("directores"); //Botón premiados
+
+//Botón Regresar//
+const regresarBtn = document.getElementById("botonReset")
