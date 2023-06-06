53806df3f1d830ad29650ad1c10021ee0f27e6d8
Kimberly Rv <elliis.rv@gmail.com>
2023-06-06 12:15:42 -0600

    imprimir en pantalla trabajado en OH

--- src/main.js
+++ src/main.js
@@ -35,6 +35,43 @@ console.log(dataFilms[i])
 
 //}
 
+//const que guarda la data de Estudio Ghibli//
+const dataFilms = obtenerPeliculas(); //OBJETO//
+
+ //function mostrarPeliculas(peliculas){
+//for (let i=0; i < dataFilms.length; i++) 
+//console.log(dataFilms[i]);
+ //}
+
+//const dataFilms = data.films; //Animaciones// ARRAY//
+//console.log(dataFilms);
+const filmsList = document.getElementById("filmsList"); //Lista de animaciones
+const listaPeliculas = document.getElementById("listaPeliculas");
+//Iterar en films para cada categoría// Muestra cada elemento del array
+for (let i=0; i < dataFilms.length; i++) {
+  //console.log(dataFilms[i])
+  const dataDirectors = dataFilms[i].director; //Directores
+  //console.log(dataFilms[i].title + ": " + "Director: " + dataDirectors); //Concatena el título de la película con el director
+
+  const dataYear = dataFilms[i].release_date; //Año
+  //console.log(dataFilms[i].title + ": " + "Año: " + dataYear);
+
+  const dataScore = dataFilms[i].rt_score; //Duración
+  //console.log(dataFilms[i].title + ": " + "Duración: " + dataScore + " minutos");
+  const filmName = dataFilms[i].title;
+  const element = document.createElement("li")
+  element.innerText=dataFilms[i]
+  filmsList.appendChild(element)
+  const imagen = document.createElement("img")
+  imagen.src = dataFilms[i].poster
+  imagen.classList.add("imagen")
+  listaPeliculas.appendChild(imagen)
+  const nombre= document.createElement("h3")
+  nombre.innerText=filmName
+  listaPeliculas.appendChild(nombre)
+  
+}
+
 //Header//
 const buscadorInput = document.getElementById("textInput"); //Buscador
 const buscarBtn = document.getElementById("botonBuscar"); //Botón buscar
