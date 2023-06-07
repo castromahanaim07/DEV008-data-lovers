f737012270bb663988116fa0e9c56260f3f77e73
Kimberly Rv <elliis.rv@gmail.com>
2023-06-07 15:09:39 -0600

    copia de main con los avances hasta el momento

--- src/main.js
+++ src/main.js
@@ -1,39 +1,6 @@
 import { obtenerPeliculas } from './data.js';
 //importa funcion de data
 
-import data from './data/ghibli/ghibli.js'; //Importa la data de ghibli y la imprime en consola
-//console.log(example, data);
-
-//const que guarda la data de Estudio Ghibli//
-//const dataGhibli = data; //OBJETO//
-//console.log(dataGhibli);
-
-const dataFilms = data.films; //Animaciones// ARRAY//
-console.log(dataFilms);
-
-const filmsList = document.getElementById("filmsList"); //Lista de animaciones
-filmsList.innerHTML = //Para mostrar en dcumento//
-
-//function mostrarPeliculas(dataFilms) {
-//for (let i = 0; i < dataFilms.length; i++)
-//return dataFilms[i];
-//console.log(mostrarPeliculas)
-//}
-
-
-//Iterar en films para cada categoría// Muestra cada elemento del array
-for (let i=0; i < dataFilms.length; i++) {
-console.log(dataFilms[i])
-//const dataDirectors = dataFilms[i].director; //Directores
-//console.log(dataFilms[i].title + ": " + "Director: " + dataDirectors); //Concatena el título de la película con el director
-
-//const dataYear = dataFilms[i].release_date; //Año
-//console.log(dataFilms[i].title + ": " + "Año: " + dataYear);
-
-//const dataScore = dataFilms[i].rt_score; //Duración
-//console.log(dataFilms[i].title + ": " + "Duración: " + dataScore + " minutos");
-
-//}
 
 //const que guarda la data de Estudio Ghibli//
 const dataFilms = obtenerPeliculas(); //OBJETO//
@@ -47,18 +14,6 @@ const dataFilms = obtenerPeliculas(); //OBJETO//
 //console.log(dataFilms);
 const filmsList = document.getElementById("filmsList"); //Lista de animaciones
 const listaPeliculas = document.getElementById("listaPeliculas");
-
-//variables de contenedores tarjeta html//
-const tarjetaAnimacion = document.getElementById("tarjetaAnimacion");
-const tituloTarjeta = document.getElementById("tituloTarjeta");
-const descripcionTarjeta = document.getElementById("descripcionTarjeta");
-const imagenTarjeta = document.getElementById("imagenTarjeta");
-const directorTarjeta = document.getElementById("directorTarjeta");
-const productorTarjeta = document.getElementById("productorTarjeta");
-const añoTarjeta = document.getElementById("añoTarjeta");
-const botonPersonajes = document.getElementById("botonPersonajes");
-const duracionTarjeta = document.getElementById("duracionTarjeta");
-
 //Iterar en films para cada categoría// Muestra cada elemento del array
 for (let i=0; i < dataFilms.length; i++) {
   //console.log(dataFilms[i])
@@ -84,17 +39,20 @@ for (let i=0; i < dataFilms.length; i++) {
   
 }
 
+
+
+
 //Header//
 const buscadorInput = document.getElementById("textInput"); //Buscador
 const buscarBtn = document.getElementById("botonBuscar"); //Botón buscar
 
-buscadorInput.addEventListener("input", function () {
+//buscadorInput.addEventListener("input", function () {
 
-})
+//})
 
-buscarBtn.addEventListener("input", function () {
+//buscarBtn.addEventListener("input", function () {
 
-})
+//})
 
 
 
