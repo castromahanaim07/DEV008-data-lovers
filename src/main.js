aa5960eacdc99edfd135b783d760c44f4ab5ab97
Kimberly Rv <elliis.rv@gmail.com>
2023-06-07 21:25:47 -0600

    prueba con 3 tarjetas y se agrega logo

--- src/main.js
+++ src/main.js
@@ -18,6 +18,13 @@ const tarjetaAnimacion = document.getElementById("tarjetaAnimacion");
 const textoTarjeta = document.getElementById("informacionTarjeta");
 const imagenTarjeta = document.getElementById("imagenTarjeta");
 //const descripcionTarjeta = document.getElementById("descripcionTarjeta");
+const tarjetaAnimacion1 = document.getElementById("tarjetaAnimacion1");
+const textoTarjeta1 = document.getElementById("informacionTarjeta1");
+const imagenTarjeta1 = document.getElementById("imagenTarjeta1");
+
+const tarjetaAnimacion2 = document.getElementById("tarjetaAnimacion2");
+const textoTarjeta2 = document.getElementById("informacionTarjeta2");
+const imagenTarjeta2 = document.getElementById("imagenTarjeta2");
 
 //for (let i = 0; i < dataFilms.length; i++) {
 //   const element = document.createElement("li")
@@ -30,18 +37,62 @@ imagenTarjeta.appendChild(imagen)
 const nombre = document.createElement("h2")
 nombre.innerText = dataFilms[0].title;
 textoTarjeta.appendChild(nombre)
-const director = document.createElement ("h4")
+const director = document.createElement ("p")
 director.innerText = "Director: " + dataFilms[0].director;
 textoTarjeta.appendChild(director)
-const productor = document.createElement ("h4")
+const productor = document.createElement ("p")
 productor.innerText = "Productor: " + dataFilms[0].producer;
 textoTarjeta.appendChild(productor)
-const año = document.createElement ("h4")
+const año = document.createElement ("p")
 año.innerText = "Año de lanzamiento: " + dataFilms[0].release_date;
 textoTarjeta.appendChild(año)
-const duracion = document.createElement ("h4")
+const duracion = document.createElement ("p")
 duracion.innerText = "Duración: " + dataFilms[0].rt_score + " minutos";
 textoTarjeta.appendChild(duracion)
+
+const imagen1 = document.createElement("img")
+imagen1.src = dataFilms[1].poster
+imagen1.classList.add("imagen1")
+imagenTarjeta1.appendChild(imagen1)
+const nombre1 = document.createElement("h2")
+nombre1.innerText = dataFilms[1].title;
+textoTarjeta1.appendChild(nombre1)
+const director1 = document.createElement ("p")
+director1.innerText = "Director: " + dataFilms[1].director;
+textoTarjeta1.appendChild(director1)
+const productor1 = document.createElement ("p")
+productor1.innerText = "Productor: " + dataFilms[1].producer;
+textoTarjeta1.appendChild(productor1)
+const año1 = document.createElement ("p")
+año1.innerText = "Año de lanzamiento: " + dataFilms[1].release_date;
+textoTarjeta1.appendChild(año1)
+const duracion1 = document.createElement ("p")
+duracion1.innerText = "Duración: " + dataFilms[1].rt_score + " minutos";
+textoTarjeta1.appendChild(duracion1)
+
+const imagen2 = document.createElement("img")
+imagen2.src = dataFilms[2].poster
+imagen2.classList.add("imagen2")
+imagenTarjeta2.appendChild(imagen2)
+const nombre2 = document.createElement("h2")
+nombre2.innerText = dataFilms[2].title;
+textoTarjeta2.appendChild(nombre2)
+const director2 = document.createElement ("p")
+director2.innerText = "Director: " + dataFilms[2].director;
+textoTarjeta2.appendChild(director2)
+const productor2 = document.createElement ("p")
+productor2.innerText = "Productor: " + dataFilms[2].producer;
+textoTarjeta2.appendChild(productor2)
+const año2 = document.createElement ("p")
+año2.innerText = "Año de lanzamiento: " + dataFilms[2].release_date;
+textoTarjeta2.appendChild(año2)
+const duracion2 = document.createElement ("p")
+duracion2.innerText = "Duración: " + dataFilms[2].rt_score + " minutos";
+textoTarjeta2.appendChild(duracion2)
+
+
+
+
 // const descripcion = document.createElement ("h4")  //NOTA: ESTA EN INGLES//
 // descripcion.innerText = "Descripción: " + dataFilms[0].description;
 // textoTarjeta.appendChild(descripcion)
