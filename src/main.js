b8bf09e8704ceafefd22f26ca4ec8e851827a040
Kimberly Rv <elliis.rv@gmail.com>
2023-06-08 13:54:51 -0600

    agrego funcion crearTarjetas

--- src/main.js
+++ src/main.js
@@ -5,17 +5,17 @@ import { obtenerPeliculas } from './data.js';
 //const que guarda la data de Estudio Ghibli//
 const dataFilms = obtenerPeliculas(); //OBJETO//
 
- //function mostrarPeliculas(peliculas){
+//function mostrarPeliculas(peliculas){
 //for (let i=0; i < dataFilms.length; i++) 
 //console.log(dataFilms[i]);
- //}
+//}
 
 //const dataFilms = data.films; //Animaciones// ARRAY//
 //console.log(dataFilms);
 const filmsList = document.getElementById("filmsList"); //Lista de animaciones
 const listaPeliculas = document.getElementById("listaPeliculas");
 //Iterar en films para cada categoría// Muestra cada elemento del array
-for (let i=0; i < dataFilms.length; i++) {
+for (let i = 0; i < dataFilms.length; i++) {
   //console.log(dataFilms[i])
   const dataDirectors = dataFilms[i].director; //Directores
   //console.log(dataFilms[i].title + ": " + "Director: " + dataDirectors); //Concatena el título de la película con el director
@@ -25,21 +25,55 @@ for (let i=0; i < dataFilms.length; i++) {
 
   const dataScore = dataFilms[i].rt_score; //Duración
   //console.log(dataFilms[i].title + ": " + "Duración: " + dataScore + " minutos");
-  const filmName = dataFilms[i].title;
-  const element = document.createElement("li")
-  element.innerText=dataFilms[i]
-  filmsList.appendChild(element)
-  const imagen = document.createElement("img")
-  imagen.src = dataFilms[i].poster
-  imagen.classList.add("imagen")
-  listaPeliculas.appendChild(imagen)
-  const nombre= document.createElement("h3")
-  nombre.innerText=filmName
-  listaPeliculas.appendChild(nombre)
-  
-}
 
+  function crearTarjetas(films) {
 
+    for (let i = 0; i < films.length; i++) {
+
+      const contenedorTarjetas = document.createElement("div")
+      contenedorTarjetas.classList.add("container")
+      listaPeliculas.appendChild(contenedorTarjetas)
+
+      const tarjeta = document.createElement("section")
+      tarjeta.classList.add("contenedorTarjeta")
+      contenedorTarjetas.appendChild(tarjeta)
+
+      const contenedorImagen = document.createElement("section")
+      contenedorImagen.classList.add("contenedorImagen")
+      tarjeta.appendChild(contenedorImagen)
+
+      const imagen = document.createElement("img")
+      imagen.src = films[i].poster
+      contenedorImagen.appendChild(imagen)
+
+      const contenedorTexto = document.createElement("section")
+      contenedorTexto.classList.add("contenedorTexto")
+      tarjeta.appendChild(contenedorTexto)
+
+      const nombre = document.createElement("h2")
+      nombre.innerText = films[i].title;
+      contenedorTexto.appendChild(nombre)
+      const director = document.createElement("p")
+      director.innerText = "Director: " + films[i].director;
+      contenedorTexto.appendChild(director)
+      const productor = document.createElement("p")
+      productor.innerText = "Productor: " + films[i].producer;
+      contenedorTexto.appendChild(productor)
+      const año = document.createElement("p")
+      año.innerText = "Año de lanzamiento: " + films[i].release_date;
+      contenedorTexto.appendChild(año)
+      const duracion = document.createElement("p")
+      duracion.innerText = "Duración: " + films[i].rt_score + " minutos";
+      contenedorTexto.appendChild(duracion)
+
+      contenedorTexto.appendChild(nombre)
+      contenedorTexto.appendChild(director)
+      contenedorTexto.appendChild(productor)
+      contenedorTexto.appendChild(año)
+      contenedorTexto.appendChild(duracion)
+    }
+  }
+  crearTarjetas(dataFilms);
 
 
 //Header//
