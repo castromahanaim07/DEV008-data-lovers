03a8a865978b18166ebd42a6b5e1e6fe11dafbd6
Kimberly Rv <elliis.rv@gmail.com>
2023-06-07 19:22:22 -0600

    prueba de tarjeta1

--- src/main.js
+++ src/main.js
@@ -1,43 +1,54 @@
 import { obtenerPeliculas } from './data.js';
 //importa funcion de data
 
+//const que guarda el array de Films//
+const dataFilms = obtenerPeliculas();
 
-//const que guarda la data de Estudio Ghibli//
-const dataFilms = obtenerPeliculas(); //OBJETO//
-
- //function mostrarPeliculas(peliculas){
+//function mostrarPeliculas(peliculas){
 //for (let i=0; i < dataFilms.length; i++) 
 //console.log(dataFilms[i]);
- //}
+//}
 
 //const dataFilms = data.films; //Animaciones// ARRAY//
 //console.log(dataFilms);
 const filmsList = document.getElementById("filmsList"); //Lista de animaciones
 const listaPeliculas = document.getElementById("listaPeliculas");
-//Iterar en films para cada categoría// Muestra cada elemento del array
-for (let i=0; i < dataFilms.length; i++) {
-  //console.log(dataFilms[i])
-  const dataDirectors = dataFilms[i].director; //Directores
-  //console.log(dataFilms[i].title + ": " + "Director: " + dataDirectors); //Concatena el título de la película con el director
+//variables de contenedores tarjeta html//
+const tarjetaAnimacion = document.getElementById("tarjetaAnimacion");
+const textoTarjeta = document.getElementById("informacionTarjeta");
+const imagenTarjeta = document.getElementById("imagenTarjeta");
+//const descripcionTarjeta = document.getElementById("descripcionTarjeta");
 
-  const dataYear = dataFilms[i].release_date; //Año
-  //console.log(dataFilms[i].title + ": " + "Año: " + dataYear);
+//for (let i = 0; i < dataFilms.length; i++) {
+//   const element = document.createElement("li")
+//   element.innerText = dataFilms[i]
+//   filmsList.appendChild(element)
+const imagen = document.createElement("img")
+imagen.src = dataFilms[0].poster
+imagen.classList.add("imagen")
+imagenTarjeta.appendChild(imagen)
+const nombre = document.createElement("h2")
+nombre.innerText = dataFilms[0].title;
+textoTarjeta.appendChild(nombre)
+const director = document.createElement ("h4")
+director.innerText = "Director: " + dataFilms[0].director;
+textoTarjeta.appendChild(director)
+const productor = document.createElement ("h4")
+productor.innerText = "Productor: " + dataFilms[0].producer;
+textoTarjeta.appendChild(productor)
+const año = document.createElement ("h4")
+año.innerText = "Año de lanzamiento: " + dataFilms[0].release_date;
+textoTarjeta.appendChild(año)
+const duracion = document.createElement ("h4")
+duracion.innerText = "Duración: " + dataFilms[0].rt_score + " minutos";
+textoTarjeta.appendChild(duracion)
+// const descripcion = document.createElement ("h4")  //NOTA: ESTA EN INGLES//
+// descripcion.innerText = "Descripción: " + dataFilms[0].description;
+// textoTarjeta.appendChild(descripcion)
 
-  const dataScore = dataFilms[i].rt_score; //Duración
-  //console.log(dataFilms[i].title + ": " + "Duración: " + dataScore + " minutos");
-  const filmName = dataFilms[i].title;
-  const element = document.createElement("li")
-  element.innerText=dataFilms[i]
-  filmsList.appendChild(element)
-  const imagen = document.createElement("img")
-  imagen.src = dataFilms[i].poster
-  imagen.classList.add("imagen")
-  listaPeliculas.appendChild(imagen)
-  const nombre= document.createElement("h3")
-  nombre.innerText=filmName
-  listaPeliculas.appendChild(nombre)
-  
-}
+//}
+
+
 
 
 
@@ -95,3 +106,18 @@ vehiculosBtn.addEventListener("click", function () {
 premiadosBtn.addEventListener("click", function () {
 
 })
+
+//FILTRAR//
+
+//Iterar en films para cada categoría// Muestra cada elemento del array
+for (let i = 0; i < dataFilms.length; i++) {
+  //console.log(dataFilms[i])
+  const dataDirectors = dataFilms[i].director; //Directores
+  //console.log(dataFilms[i].title + ": " + "Director: " + dataDirectors); //Concatena el título de la película con el director
+
+  const dataYear = dataFilms[i].release_date; //Año
+  //console.log(dataFilms[i].title + ": " + "Año: " + dataYear);
+
+  const dataScore = dataFilms[i].rt_score; //Duración
+  //console.log(dataFilms[i].title + ": " + "Duración: " + dataScore + " minutos");
+}
