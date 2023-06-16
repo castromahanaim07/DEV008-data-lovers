1a19bb9149889c43bee853511e466cb536604169
Kimberly Rv <elliis.rv@gmail.com>
2023-06-16 01:32:17 -0600

    botón buscar ya imprime resultados en la página del filtro directores

--- src/main.js
+++ src/main.js
@@ -5,61 +5,11 @@ const dataFilms = obtenerPeliculas();
 //Contenedor de resultador//
 const listaPeliculas = document.getElementById("listaPeliculas");
 
-function crearTarjetas(films) {
-
-  for (let i = 0; i < films.length; i++) {
-
-    const contenedorTarjetas = document.createElement("div")
-    contenedorTarjetas.classList.add("container")
-    listaPeliculas.appendChild(contenedorTarjetas)
-
-    const tarjeta = document.createElement("section")
-    tarjeta.classList.add("contenedorTarjeta")
-    contenedorTarjetas.appendChild(tarjeta)
-
-    const contenedorImagen = document.createElement("section")
-    contenedorImagen.classList.add("contenedorImagen")
-    tarjeta.appendChild(contenedorImagen)
-
-    const imagen = document.createElement("img")
-    imagen.src = films[i].poster
-    contenedorImagen.appendChild(imagen)
-
-    const contenedorTexto = document.createElement("section")
-    contenedorTexto.classList.add("contenedorTexto")
-    tarjeta.appendChild(contenedorTexto)
-
-    const nombre = document.createElement("h2")
-    nombre.innerText = films[i].title;
-    contenedorTexto.appendChild(nombre)
-    const director = document.createElement("p")
-    director.innerText = "Director: " + films[i].director;
-    contenedorTexto.appendChild(director)
-    const productor = document.createElement("p")
-    productor.innerText = "Productor: " + films[i].producer;
-    contenedorTexto.appendChild(productor)
-    const año = document.createElement("p")
-    año.innerText = "Año de lanzamiento: " + films[i].release_date;
-    contenedorTexto.appendChild(año)
-    const duracion = document.createElement("p")
-    duracion.innerText = "Duración: " + films[i].rt_score + " minutos";
-    contenedorTexto.appendChild(duracion)
-
-    contenedorTexto.appendChild(nombre)
-    contenedorTexto.appendChild(director)
-    contenedorTexto.appendChild(productor)
-    contenedorTexto.appendChild(año)
-    contenedorTexto.appendChild(duracion)
-  }
-}
-crearTarjetas(dataFilms);
-
 //Categorías//
 const animacionesBtn = document.getElementById("animaciones")//Botón TODAS las animaciones
 const directoresBtn = document.getElementById("directores")//Botón directores
 const duracionBtn = document.getElementById("duracion"); //Botón duración
 const añoBtn = document.getElementById("año"); //Botón año
-
 
 // //Prueba Mana//
 //     console.log (e.target.matches(textoIntroducido))
@@ -257,8 +207,11 @@ buscadorInput.addEventListener("keyup", function () {
 
 const buscarBtn = document.getElementById("botonBuscar"); //Botón buscar
 
-buscarBtn.addEventListener("input", function () {
-
+buscarBtn.addEventListener("click", function () {
+  const textoIntroducido = buscadorInput.value;
+  const directoresFiltrados = filtrarDirectores(dataFilms, textoIntroducido)
+  document.getElementById('listaPeliculas').innerHTML = '';
+  animacionesTarjeta(directoresFiltrados);
 })
 
 //Botón limpiar filtro //
